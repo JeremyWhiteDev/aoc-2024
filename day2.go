71f470f4d2a60e53f5adcfde5c93d00a19be1b55
Jeremy White <jcwhite414@gmail.com>
2024-12-04 11:06:14 -0600

    day2Part2 failing

--- day2.go
+++ day2.go
@@ -33,7 +33,6 @@ func dayTwoPartOne() int {
 		reportArr := strings.Fields(report)
 		reportIsSafe := true
 		reportShouldIncrease := false
-		
 
 		for i, currentLevel := range reportArr {
 			// we're done with this report
@@ -60,5 +59,96 @@ func dayTwoPartOne() int {
 		}
 	}
 	return safeReports
+}
 
+// incorrect currently
+func dayTwoPartTwo() int {
+	s := getData("2")
+
+	allReports := strings.FieldsFunc(s, func (r rune) bool {
+		return r == '\n'
+	})
+
+	safeReports := 0
+
+	for _, report := range allReports {
+		reportArr := strings.Fields(report)
+
+		if reportIsSafe(reportArr, false) {
+			safeReports++
+		}
+	}
+	return safeReports
+}
+
+func reportIsSafe(reportArr []string, skipRecursion bool) bool {
+	reportShouldIncrease := false
+	numOfLevelIncreases := 0
+	numOfLevelDecreases := 0
+	var unsafeLevels []int
+
+	// Determine whether a report is expected to increase or decrease.
+	// 1 1 2
+	// 20 22 19 18 17
+	// 20 21 19 23 24
+	for i, currentLevel := range reportArr {
+		if i == 0 { continue }
+		currentLevel := must(strconv.Atoi(currentLevel))
+		prevLevel := must(strconv.Atoi(reportArr[i -1]))
+		if currentLevel > prevLevel {
+			numOfLevelIncreases++
+		}
+		if currentLevel < prevLevel {
+			numOfLevelDecreases++
+		}
+	}
+
+	reportShouldIncrease = numOfLevelIncreases > numOfLevelDecreases
+
+	// iterate over levels and find those levels which are unsafe
+	for i, currentLevel := range reportArr {
+		if i == 0 { continue }
+		currentLevel := must(strconv.Atoi(currentLevel))
+		prevLevel := must(strconv.Atoi(reportArr[i -1]))
+		
+		if levelIsNotSafe(currentLevel, prevLevel, reportShouldIncrease) {
+			unsafeLevels = append(unsafeLevels, i)
+		}
+	}
+
+	if len(unsafeLevels) == 0 {
+		return true
+	}
+
+	// brute force retry
+
+	if skipRecursion {
+		return false
+	}
+	
+	// check if removing any of the levels makes the report safe
+	for _, unsafeLevelIndex := range unsafeLevels {
+		newReport := RemoveIndex(reportArr, unsafeLevelIndex)
+		if reportIsSafe(newReport, true) {
+			return true
+		}
+		continue
+	}
+	return false
+}
+
+// level is safe if
+// The level increases/decreases as expected
+// adjacent levels differ by minimum 1 and maximum 3
+func levelIsNotSafe(currentLevel, prevLevel int, reportShouldIncrease bool) bool {
+	prevLevelDiff := math.Abs(float64(currentLevel - prevLevel))
+	return ((reportShouldIncrease && currentLevel < prevLevel) ||
+		(!reportShouldIncrease && currentLevel > prevLevel) ||
+		(prevLevelDiff < 1 || prevLevelDiff > 3))
+}
+
+func RemoveIndex(s []string, index int) []string {
+    ret := make([]string, 0)
+    ret = append(ret, s[:index]...)
+    return append(ret, s[index+1:]...)
 }
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	fmt.Println("Day 1 Part 1 Answer: ", dayOnePartOne())
 	fmt.Println("Day 1 Part 2 Answer: ", dayOnePartTwo())
 	fmt.Println("Day 2 Part 1 Answer: ", dayTwoPartOne())
+	fmt.Println("Day 2 Part 2 Answer: ", dayTwoPartTwo())
 
 }
 
